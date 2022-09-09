ae7c0f8e12293e5bb05d43d5384ccf1e4f5f84e9
Valeria Vitvytska <leranmu@gmail.com>
2022-09-09 15:52:24 +0300

    feat:backbutton notfound page

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -3,6 +3,7 @@ import {Routes, Route} from 'react-router-dom'
 import FavoritesPage from './componentns/favoritespage/favorites';
 import CartPage from './componentns/cart/cart';
 import ProductPage from './componentns/productpage/productPage';
+import NotFoundPage from './componentns/notfoundpage/notfoundpange';
 
 const AppRoutes = ({products,openModal,addFavoritesFunc,addFavorites,deleteCartItem,openDeleteModal,setCartId}) => {
     return (
@@ -12,6 +13,7 @@ const AppRoutes = ({products,openModal,addFavoritesFunc,addFavorites,deleteCartI
            <Route path='/favorites' element={<FavoritesPage  addFavorites ={addFavorites}/>} />
          
            <Route path='/cart' element={<CartPage addCards={JSON.parse(localStorage.getItem('addCards'))} deleteCartItem ={deleteCartItem} openDeleteModal={openDeleteModal} setCartId={setCartId}  />}/>
+           <Route path='*' element={<NotFoundPage />} />
          </Routes>
 
         
--- src/componentns/navigation/Navigation.js
+++ src/componentns/navigation/Navigation.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import BackButton from '../backbutton/backbutton'
 import styles from './Navigation.module.scss'
 
 
@@ -7,7 +8,7 @@ const Navigation = (props) => {
 
     return (
         <header className={styles.header}>
-            <NavLink className={styles.logo} to='/' ><h2 >Fashion store</h2 ></NavLink>
+            <NavLink className={styles.logo} to='/' ><h2 >Fashion store</h2 ></NavLink><BackButton/>
             <div className={styles.container}><NavLink className={styles.cart_link} to="/cart"><img className={styles.cart} src="https://toppng.com/uploads/preview/shopping-cart-11530997216xsrc2jr32q.png" alt="cart" /><h2> CART:{props.addCards}</h2></NavLink>
                 <NavLink className={styles.fav_link} to="/favorites"><img className={styles.favorite_img} src="./star.png" alt="" /> <h2>FAVORITES:{props.addFavoritesPage}</h2></NavLink>
 
